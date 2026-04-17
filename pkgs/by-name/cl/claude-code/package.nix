8d8fa79d28a5af12380d4210b8e5861cc5b75bf0
Hleb Shauchenka <me@marleb.org>
2026-04-17 11:47:47 +0200

    claude-code: 2.1.110 -> 2.1.112

--- pkgs/by-name/cl/claude-code/package.nix
+++ pkgs/by-name/cl/claude-code/package.nix
@@ -15,14 +15,14 @@
 }:
 buildNpmPackage (finalAttrs: {
   pname = "claude-code";
-  version = "2.1.110";
+  version = "2.1.112";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${finalAttrs.version}.tgz";
-    hash = "sha256-lotQBnIXHrp/KaXl4c2xPnEaj/CZG+f2ZpcPkAroomQ=";
+    hash = "sha256-SJJqU7XHbu9IRGPMJNUg6oaMZiQUKqJhI2wm7BnR1gs=";
   };
 
-  npmDepsHash = "sha256-7ZqRIr2EKG+jDgYceO6Bq3Hp0Hvky2AdnKhQrgjppC0=";
+  npmDepsHash = "sha256-bdkej9Z41GLew9wi1zdNX+Asauki3nT1+SHmBmaUIBU=";
 
   strictDeps = true;
 
