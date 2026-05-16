b38748e30570072a42b8283e965a8321ec070d72
Jannis <git@jannis.one>
2026-05-16 17:53:57 +0200

    maintainers: add Karrfy
    (cherry picked from commit 46bf84bd79f9d78b29bc0d542edffe24e44f9254)

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -14188,6 +14188,11 @@
     email = "nixos@karpfen.dev";
     matrix = "@karpfen:matrix.org";
   };
+  Karrfy = {
+    name = "Jannis";
+    github = "Karrfy";
+    githubId = 193750820;
+  };
   kashw2 = {
     email = "supra4keanu@hotmail.com";
     github = "kashw2";
