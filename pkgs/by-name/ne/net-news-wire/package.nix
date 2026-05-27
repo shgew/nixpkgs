a0806830ec5a49de61d990251806165c4183fdeb
Hleb Shauchenka <me@marleb.org>
2026-05-27 11:32:11 +0200

    net-news-wire: 7.0.5 -> 7.0.6
    Changelog: https://github.com/Ranchero-Software/NetNewsWire/releases/tag/mac-7.0.6

--- pkgs/by-name/ne/net-news-wire/package.nix
+++ pkgs/by-name/ne/net-news-wire/package.nix
@@ -9,11 +9,11 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "net-news-wire";
-  version = "7.0.5";
+  version = "7.0.6";
 
   src = fetchurl {
     url = "https://github.com/Ranchero-Software/NetNewsWire/releases/download/mac-${version}/NetNewsWire${version}.zip";
-    hash = "sha256-R0d2X7zj/aiuG3g2D91EQleWRR57MZacyr6HuQHWD+k=";
+    hash = "sha256-3EMJLAG+d6kbdFruGjSDrXAdNGs73o/Cb6zIoMzdjtA=";
   };
 
   sourceRoot = ".";
