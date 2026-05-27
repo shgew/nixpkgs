bcd3c9ea56081a30162c82f814753fe64eebb1e9
Hleb Shauchenka <me@marleb.org>
2026-05-27 11:20:34 +0200

    ccusage: init at 20.0.5

---
+++ pkgs/by-name/cc/ccusage/package.nix
@@ -0,0 +1,66 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  fetchurl,
+  pkg-config,
+  stdenv,
+  libiconv,
+  versionCheckHook,
+  nix-update-script,
+}:
+
+let
+  litellmPricing = fetchurl {
+    url = "https://raw.githubusercontent.com/BerriAI/litellm/e59e34bed3670a6894d43129c2af16af28057d03/model_prices_and_context_window.json";
+    hash = "sha256-aPue4NpPpTKAtAYCI8S8ojmVCDtYr+mxwtYkOASEg3w=";
+  };
+in
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "ccusage";
+  version = "20.0.5";
+
+  src = fetchFromGitHub {
+    owner = "ryoppippi";
+    repo = "ccusage";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-BbjNh3yHu/Cn6edKLMzj+2GfjZpZxplM56qYx0+SmJQ=";
+  };
+
+  sourceRoot = "${finalAttrs.src.name}/rust";
+
+  __structuredAttrs = true;
+  strictDeps = true;
+
+  cargoHash = "sha256-ce2Zq0w0tcpgsvt7UhokajBAIi3HX421AT6RWjhkxX8=";
+
+  cargoBuildFlags = [
+    "-p"
+    "ccusage"
+    "--bin"
+    "ccusage"
+  ];
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [ libiconv ];
+
+  env.CCUSAGE_PRICING_JSON_PATH = litellmPricing;
+
+  doCheck = false;
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  doInstallCheck = true;
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Analyze coding agent CLI token usage and costs from local data";
+    homepage = "https://github.com/ryoppippi/ccusage";
+    changelog = "https://github.com/ryoppippi/ccusage/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ shgew ];
+    mainProgram = "ccusage";
+    platforms = lib.platforms.unix;
+  };
+})
