7b61c495a23387c9eecae0df970c2a71246093b4
Hleb Shauchenka <me@marleb.org>
2026-06-10 08:37:23 +0200

    opencode: 1.16.2 -> 1.17.0

--- pkgs/by-name/op/opencode/package.nix
+++ pkgs/by-name/op/opencode/package.nix
@@ -16,7 +16,7 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "opencode";
-  version = "1.16.2";
+  version = "1.17.0";
 
   __structuredAttrs = true;
   strictDeps = true;
@@ -25,7 +25,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     owner = "anomalyco";
     repo = "opencode";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-IpTD4YCgGNtYlZ6EoyY+YLD81rIFR0D2A4W3uhWSSfo=";
+    hash = "sha256-3JxIc7I5uNswGyaPa+MTGdNWCU4p3CFm+8/bICh0jsA=";
   };
 
   node_modules = stdenvNoCC.mkDerivation {
@@ -78,7 +78,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     # NOTE: Required else we get errors that our fixed-output derivation references store paths
     dontFixup = true;
 
-    outputHash = "sha256-4yjQlxN+U4CKwA/hE8gACuvA4bBeTrX0ACVBIK4UQCg=";
+    outputHash = "sha256-oHy+WKRPAxHbiAOHj0wrllXG297dcfQQw8gR1ds9Ano=";
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
   };
